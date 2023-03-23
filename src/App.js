4fd6007cfafd47ca8cdc82d5a21e824bdac3c6a2
Shalu-patidar <shalupatidar155@gmail.com>
2023-03-23 11:14:09 +0000

    matchGame

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import './App.css'
+
+// These are the lists used in the application. You can move them to any component needed.
+import EmojiGame from './components/EmojiGame'
+// Replace your code here
+const App = () => <EmojiGame />
+
+export default App
